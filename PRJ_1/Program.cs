2db7505b8e138e76c84ff66fdf966c5418899428
Nikolay Rusanov <nikolarusano92@gmail.com>
2020-04-22 22:22:07 +0300

    Add project files.

---
+++ PRJ_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRJ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
